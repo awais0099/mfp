93e682feb9f8fbdf9f8c6ce100531bb92f47879a
awais <awaishu.se@gmail.com>
2024-08-13 17:50:57 +0500

    initial, github actions workflow script created

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Marketing from "./components/Marketing";
+
+export default () => {
+    return (
+        <div>
+            <h1>Container</h1>
+            <hr />
+            <Marketing />
+        </div>
+    )
+}
