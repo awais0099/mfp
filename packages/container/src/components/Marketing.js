efb1fee0d4c516900ac7805ba060992784597030
awais <awaishu.se@gmail.com>
2024-08-14 19:28:50 +0500

    built communication b/w container and marketing micro frontend app for navigation

--- packages/container/src/components/Marketing.js
+++ packages/container/src/components/Marketing.js
@@ -1,11 +1,23 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
     const ref = useRef(null);
-    
+    const history = useHistory();
+
     useEffect(() => {
-        mount(ref.current);
+        const { onParentNavigate } = mount(ref.current, {
+            onNavigate: (location) => {
+                console.log('The container noticed navigation in Marketing.');
+                const { pathname } =  history.location;
+                if (pathname != location.pathname) {
+                    history.push(location.pathname);
+                }
+            },
+        });
+
+        history.listen(onParentNavigate);
     }, []);
 
     return <div ref={ref} />
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el) => {
-    ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+    
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate(location) {
+            console.log('Container just navigated');
+            const { pathname } = history.location;
+
+            if (pathname != location.pathname) {
+                history.push(location.pathname);
+            }
+        },
+    };
 }
 
 if (process.env.NODE_ENV === 'development') {
+    console.log('alon');
+    console.log(process.env.NODE_ENV);
+
     const devroot = document.querySelector('#marketing-dev-root');
     if (devroot) {
-        mount(devroot);
+        mount(devroot, {defaultHistory: createBrowserHistory()});
     }
 }
 
